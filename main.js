1a409cb02475ba20dc74c7ef12836b1e8e8175ff
DouglasLopesDRL <douglasribeirodrl@gmail.com>
2025-08-14 22:00:56 -0300

    final change main.js

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
 const form = document.getElementById('formulario');
+const inputNomeContato = document.getElementById('nome-contato');
+const inputTelefoneContato = document.getElementById('telefone-contato');
+let linhas = '';
+const telefones = [];
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    const inputNomeContato = document.getElementById('nome-contato');
-    const inputTelefoneContato = document.getElementById('telefone-contato');
+    adcionarLinha();
+    atualizarTabela();
 });
+
+function adcionarLinha(){
+    if(telefones.includes(inputTelefoneContato.value)){
+        alert(`Este telefone: ${inputTelefoneContato.value} já está vinculado a um contato!!`);
+        inputTelefoneContato.value = '';
+    } else{
+        telefones.push(inputTelefoneContato.value);
+        let linha = '<tr>';
+        linha += `<td>${inputNomeContato.value}</td>`;
+        linha += `<td>${inputTelefoneContato.value}</td>`;
+        linha += '</tr>'
+        linhas += linha;
+        limparCampos();
+    }
+}
+
+function atualizarTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+function limparCampos(){
+    inputNomeContato.value = '';
+    inputTelefoneContato.value = '';
+}
